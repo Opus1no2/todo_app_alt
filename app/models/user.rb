b7adc6a037a272446eb82ab86a1e601a5eee2993
Opus1no2 <tillotson.travis@gmail.com>
2021-12-20 07:10:22 -0600

    default todo creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   has_many :todo_lists
   has_many :todos, through: :todo_lists
+
+  after_create :create_default_list
+
+  def create_default_list
+    todo_list = TodoList.create!(label: "Daily", user: self)
+    Todo.create!(description: "Get Started!", todo_list: todo_list)
+  end
 end
