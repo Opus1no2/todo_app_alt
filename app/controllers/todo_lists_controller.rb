02af31eaa954c6537ebcbb588f5170f34f0c0d8c
Opus1no2 <tillotson.travis@gmail.com>
2021-12-29 15:50:49 -0600

    create new lists

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -7,6 +7,22 @@ class TodoListsController < ApplicationController
     @todo_lists = current_user.todo_lists
   end
 
+  def create
+    @todo_list = TodoList.new(
+      label: todo_list_params[:label],
+      user: current_user
+    )
+
+    respond_to do |format|
+      format.turbo_stream do
+        if @todo_list.save
+          render turbo_stream: turbo_stream.append(:todo_lists, partial: 'shared/todo_list',
+                                                            locals: { todo_list: @todo_list })
+        end
+      end
+    end
+  end
+
   def destroy
     @todo_list = todos_view.todo_lists.find(params[:id])
     @todo_list.destroy
@@ -17,6 +33,10 @@ class TodoListsController < ApplicationController
   end
 
   private
+
+  def todo_list_params
+    params.require(:todo_list).permit(:label)
+  end
 
   def todos_view
     @todos_view ||= TodosView.new(current_user, params)
