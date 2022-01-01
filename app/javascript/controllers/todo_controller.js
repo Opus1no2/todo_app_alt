a62084f6110219a2cd0f65dfb8822c52304ea948
Opus1no2 <tillotson.travis@gmail.com>
2022-01-01 00:11:42 -0600

    properly submit todo input form

--- app/javascript/controllers/todo_controller.js
+++ app/javascript/controllers/todo_controller.js
@@ -3,14 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input"]
 
-  initialize() {
+  connect() {
     this.formTarget.addEventListener("turbo:submit-end", () => {
       this.inputTarget.value = null;
-    })
-    this.inputTarget.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        this.formTarget.requestSubmit();
-      }
-    })
+    });
+  }
+  submitForm(e) {
+    if (e.key === "Enter") {
+      this.formTarget.requestSubmit();
+    }
   }
 }
